12c4a1beab9b4335c1d3a565be6a519fb117f9e4
Max Claus Nunes <maxcnunes@gmail.com>
2023-10-30 00:03:01 -0300

    Handle path

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use std::io::BufReader;
 use std::io::{BufRead, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -11,14 +12,23 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                println!("accepted new connection");
-                handle_connection(stream);
+                thread::spawn(|| {
+                    println!("accepted new connection ({})", stream.peer_addr().unwrap());
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+#[derive(Debug)]
+struct Request {
+    method: String,
+    path: String,
+    http_info: String,
 }
 
 // TODO: handle errors properly
@@ -28,16 +38,37 @@ fn handle_connection(mut stream: TcpStream) {
     // and return "Recv failure: Connection reset by peer" to the client.
 
     // Wrap stream with Bufreader
-    let mut reader = BufReader::new(&mut stream);
+    let reader = BufReader::new(&mut stream);
+
+    let mut req = Request {
+        method: String::new(),
+        path: String::new(),
+        http_info: String::new(),
+    };
 
     // Read all data from this stream
-    let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
+    for (i, l) in reader.lines().enumerate() {
+        let line = l.unwrap();
+        println!("line {:?}", line);
+        if i == 0 {
+            let parts: Vec<&str> = line.split(" ").collect();
+            if parts.len() != 3 {
+                panic!("Bad first line format {:?}", parts);
+            }
 
-    // Could run some validation based on the income data here...
+            req.method = parts[0].to_string();
+            req.path = parts[1].to_string();
+            req.http_info = parts[2].to_string();
+        }
+        if line == "" {
+            // End of the request data
+            break;
+        }
+    }
 
-    // Tells this buffer that amt bytes have been consumed from the buffer,
-    // so they should no longer be returned in calls to read.
-    reader.consume(received.len());
+    println!("Request {:?}", req);
+
+    println!("Responding");
 
     // Write response:
     //
@@ -48,7 +79,13 @@ fn handle_connection(mut stream: TcpStream) {
     // * The first "\r\n" signifies the end of the status line.
     // * The second "\r\n" signifies the end of the response headers section (which is empty in this case).
     //
-    let buf = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
-    stream.write(buf).unwrap();
+    let status = match req.path.as_str() {
+        "/" => "200 OK",
+        _ => "404 Not Found",
+    };
+
+    let res_content = format!("HTTP/1.1 {}\r\n\r\n", status);
+    stream.write(&res_content.into_bytes()).unwrap();
     stream.flush().unwrap();
+    println!("Done");
 }
